edff24f0f0a82bce4dcc17369309b15ebafb061b
Ascen Keeprov <32298380+AscenKeeprov@users.noreply.github.com>
2017-09-27 10:34:58 +0300

    Exercise3 upload

---
+++ Exercise3-GitHub/RepoPrime/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program
+{
+	class Startup
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Please enter your name: ");
+			string name = Console.ReadLine().Trim();
+			Console.WriteLine($"░░▒▒▓▓ Welcome to GitHub, {name}! ▓▓▒▒░░");
+		}
+	}
+}
