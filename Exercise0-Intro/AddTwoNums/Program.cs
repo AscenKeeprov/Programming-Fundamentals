59c6e3e6e23019c400f69679c90487ec991f2375
Ascen Keeprov <32298380+AscenKeeprov@users.noreply.github.com>
2017-09-27 10:04:10 +0300

    Exercise0 upload

---
+++ Exercise0-Intro/AddTwoNums/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddTwoNums
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var num1 = int.Parse(Console.ReadLine());
+			var num2 = int.Parse(Console.ReadLine());
+			var sum = num1 + num2;
+			Console.WriteLine($"{num1} + {num2} = {sum}");
+		}
+	}
+}
---
+++ Exercise0-Intro/EmployeeData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmployeeData
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = Console.ReadLine();
+			var age = int.Parse(Console.ReadLine());
+			var ID = int.Parse(Console.ReadLine());
+			var salary = double.Parse(Console.ReadLine());
+			Console.WriteLine($"Name: {name}");
+			Console.WriteLine($"Age: {age}");
+			Console.WriteLine($"Employee ID: {ID:D8}");
+			Console.WriteLine($"Salary: {salary:F2}");
+		}
+	}
+}
